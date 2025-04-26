dfffbd2a205ea0740d5e987e22ebf0e3c1c13b6e
Martin Kadlec <harou2@gmail.com>
2025-04-26 19:59:49 +0200

    Use different way to cancel spawning in overworld to avoid mod conflicts

--- src/main/java/harou/phantoms_in_the_end/mixin/PhantomSpawnerMixin.java
+++ src/main/java/harou/phantoms_in_the_end/mixin/PhantomSpawnerMixin.java
@@ -3,16 +3,14 @@ package harou.phantoms_in_the_end.mixin;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.spawner.PhantomSpawner;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PhantomSpawner.class)
 public abstract class PhantomSpawnerMixin {
-    /**
-     * @author phantoms_in_the_end
-     * @reason Completely override vanilla behavior to prevent all overworld phantom spawning
-     */
-    @Overwrite
-    public int spawn(ServerWorld world, boolean spawnMonsters, boolean spawnAnimals) {
-        return 0; // Always return 0 (no phantoms spawned)
+    @Inject(method = "spawn", at = @At("HEAD"), cancellable = true)
+    private void onSpawn(ServerWorld world, boolean spawnMonsters, boolean spawnAnimals, CallbackInfoReturnable<Integer> ci) {
+        ci.cancel();
     }
 }
